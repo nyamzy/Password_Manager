02be27d7d793b2066f0c91af2b70f0669ef0b3aa
nyamzy <john.nyamweya@student.moringaschool.com>
2021-08-02 12:54:54 +0300

    create classes user and credentials and define properties for objects in user class

---
+++ password_manager.py
@@ -0,0 +1,20 @@
+class User:
+  '''
+  Class that generates new instances of users
+  '''
+  def __init__(self, first_name, last_name, user_name, email, password):
+    '''
+    __init__ method that defines properties for our objects
+    '''
+    self.first_name = first_name
+    self.last_name = last_name
+    self.user_name = user_name
+    self.email = email
+    self.password = password
+
+class Credentials:
+  '''
+  Class that generates new instances of credentials for users
+  '''
+  pass
+
