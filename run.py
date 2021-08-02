d72adf6069d4d7934d3599e46858ddd1773a601c
nyamzy <john.nyamweya@student.moringaschool.com>
2021-08-02 14:50:58 +0300

    create function that saves a user

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 from password_manager import User
 
+def create_user(fname, lname, uname, email, password):
+  '''
+  Function that creates a new user
+  '''
+  new_user = User(fname, lname, uname, email, password)
+  return new_user
+
+def save_users(user):
+  '''
+  Function that saves a new user
+  '''
+  user.save_user()
+  
+
