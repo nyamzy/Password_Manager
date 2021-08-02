5c369150122194e901bd8c2f9f7c5858be32611e
nyamzy <john.nyamweya@student.moringaschool.com>
2021-08-02 13:06:32 +0300

    create test sublass for user behaviours

--- password_manager_test.py
+++ password_manager_test.py
@@ -1,1 +1,8 @@
 import unittest
+from password_manager import User, Credentials
+
+class TestUser(unittest.TestCase):
+  '''
+  Subclass for testing the cases for the user class behaviours
+  '''
+  
