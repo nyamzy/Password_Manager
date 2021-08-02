7629243728baef946ca01afe23a6594c75703b57
nyamzy <john.nyamweya@student.moringaschool.com>
2021-08-02 13:13:34 +0300

    create setup method

--- password_manager_test.py
+++ password_manager_test.py
@@ -5,4 +5,9 @@ class TestUser(unittest.TestCase):
   '''
   Subclass for testing the cases for the user class behaviours
   '''
-  
+  def setUp(self):
+    '''
+    Set up method that runs before each test case
+    '''
+    self.new_user = User("John", "Nyamweya", "0712345678", "johnnyamweya@gmail.com", "hello123",) #create new user object
+    
